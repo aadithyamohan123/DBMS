7ded74cab94cd275bf9149386ef4fae930e37bef
Aadithya Mohan <111652739+aadithyamohan123@users.noreply.github.com>
2024-11-12 20:36:43 +0530

    Adding alternate approach

--- fetchValues.sql
+++ fetchValues.sql
@@ -47,3 +47,52 @@ Executing query: select EMP_ID || chr(9) || NAME || chr(9) || DEPARTMENT || chr(
 3	Sam Brown	IT	7000
 
 */
+
+--ALTERNATIVE APPROACH (USING REF CURSOR)
+
+
+create or replace procedure fetchvalues (tbl in varchar2 , condt in varchar2)
+is
+  sqlquery varchar2(1000);
+  type ref_cur is ref cursor;
+  cur ref_cur;
+begin
+  sqlquery := 'select EMP_ID, NAME, DEPARTMENT, SALARY from ' || tbl || ' where ' || condt;
+  
+  open cur for sqlquery;
+  
+  loop
+    declare
+      v_empid EMPLOYEES.EMP_ID%type;
+      v_name EMPLOYEES.NAME%type;
+      v_dept EMPLOYEES.DEPARTMENT%type;
+      v_sal EMPLOYEES.SALARY%type;
+    
+    begin
+      fetch cur into v_empid,v_name,v_dept,v_sal;
+      exit when cur%notfound;
+      
+      dbms_output.put_line(v_empid||chr(9)||v_name||chr(9)||v_dept||chr(9)||v_sal);
+    end;
+  end loop;
+  
+  close cur;
+exception
+  when others then
+    dbms_output.put_line('Error has occured: '||sqlerrm);
+    
+    if cur%isopen then
+      close cur;
+    end if;
+    
+end fetchvalues;
+/
+
+
+
+begin
+  fetchvalues('EMPLOYEES','SALARY > (select avg(SALARY) from EMPLOYEES)');
+end;
+/
+
+
